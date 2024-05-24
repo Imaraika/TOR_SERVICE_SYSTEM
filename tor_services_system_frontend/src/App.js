f43fa7e681e09839313a27bdd6f45d20cbb9d1a9
Ange <ange.ingabire07@gmail.com>
2024-05-24 05:38:44 +0200

    the company details page has been created successfully

--- tor_services_system_frontend/src/App.js
+++ tor_services_system_frontend/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
+import CompanyDetailsComponent from './components/CompanyDetails';
 
-const CompanyDetails = () => <div>Company Details</div>;
+
 const Landlord = () => <div>Landlord/Asset’s Owner</div>;
 const Tenant = () => <div>Tenant</div>;
 const Commissionaires = () => <div>Commissionaires</div>;
@@ -15,7 +16,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Carousel />} />
-        <Route path="/company-details" element={<CompanyDetails />} />
+        <Route path="/company-details" element={<CompanyDetailsComponent />} />
         <Route path="/landlord" element={<Landlord />} />
         <Route path="/tenant" element={<Tenant />} />
         <Route path="/commissionaires" element={<Commissionaires />} />
